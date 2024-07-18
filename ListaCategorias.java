dbd207bdb000b9934419c76d917c2e46d0827242
Doki-I <156979262+Doki-I@users.noreply.github.com>
2024-07-18 11:19:21 -0600

    ListaCategorias.java

--- ListaCategorias.java
+++ ListaCategorias.java
@@ -5,33 +5,41 @@
 package com.mycompany.bibliotica;
 
 public class ListaCategorias {
+
     private NodoListaCategorias primerNodo;
+    private NodoListaCategorias ultimoNodo;
 
     public ListaCategorias() {
         this.primerNodo = null;
+        this.ultimoNodo = null;
     }
 
-    public NodoListaCategorias getPrimerNodo() {
-        return primerNodo;
-    }
-
-    public void setPrimerNodo(NodoListaCategorias primerNodo) {
-        this.primerNodo = primerNodo;
+    public boolean estaVacia() {
+        return primerNodo == null;
     }
 
     public void agregarCategoria(DatoListaCategorias datoCategoria) {
         NodoListaCategorias nuevoNodo = new NodoListaCategorias(datoCategoria);
 
-        if (primerNodo == null) {
+        if (estaVacia()) {
             primerNodo = nuevoNodo;
-            primerNodo.setSiguiente(primerNodo);
+            ultimoNodo = nuevoNodo;
         } else {
-            NodoListaCategorias ultimo = primerNodo;
-            while (ultimo.getSiguiente() != primerNodo) {
-                ultimo = ultimo.getSiguiente();
-            }
-            ultimo.setSiguiente(nuevoNodo);
-            nuevoNodo.setSiguiente(primerNodo);
+            ultimoNodo.setSiguiente(nuevoNodo);
+            ultimoNodo = nuevoNodo;
         }
+        ultimoNodo.setSiguiente(primerNodo);
+    }
+
+    public void agregarCategoriaNovela() {
+        DatoListaCategorias categoriaNovela = new DatoListaCategorias();
+        categoriaNovela.setIdCategoria(3); //insertar o asignar un ID único para novela
+        categoriaNovela.setNombreCategoria("Novela");
+        agregarCategoria(categoriaNovela);
+    }
+
+    public NodoListaCategorias getPrimerNodo() {
+        return primerNodo;
     }
 }
+
